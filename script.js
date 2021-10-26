ca9fe518e0f80d0e42b946f43673b97b37e5dbe2
Duncan Byrne <duncanbyrne@Duncans-MacBook-Pro.local>
2021-10-25 20:04:04 -0400

    added the controls

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 // we need to use the api for the currency exchange
 
 const api = "https://api.exchangerate-api.com/v4/latest/USD";
+
+// controls
+
+var search = document.querySelector(".searchBox");
+var convert = document.querySelector(".convert");
+var fromCurrrency = document.querySelector(".from");
+var toCurrency = document.querySelector(".to");
+var finalValue = document.querySelector(".finalValue");
+var finalAmount = document.getElementById("finalAmount");
+var resultFrom;
+var resultTo;
+var searchValue;
