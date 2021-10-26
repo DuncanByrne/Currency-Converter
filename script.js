4be0060bc4d9869bbbb5932ce26c088a17c44bb5
Duncan Byrne <duncanbyrne@Duncans-MacBook-Pro.local>
2021-10-25 21:37:55 -0400

    fixed the javascript and added the second list of drop downs for all currency, webpage now displays the correct amount converted

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// we need to use the api for the currency exchange
 
+   
+// we need to use the api for the currency exchange
 const api = "https://api.exchangerate-api.com/v4/latest/USD";
 
 // controls
-
 var search = document.querySelector(".searchBox");
 var convert = document.querySelector(".convert");
 var fromCurrecy = document.querySelector(".from");
@@ -15,45 +15,46 @@ var resultTo;
 var searchValue;
 
 // changing the currency
-toCurrecy.addEventListener('change', (event) =>{
-  resultFrom = `${event.target.value}`;
+fromCurrecy.addEventListener('change', (event) => {
+	resultFrom = `${event.target.value}`;
 });
 
-toCurrecy.addEventListener('change', (event) =>{
-  resultTo = `${event.target.value}`;
+
+toCurrecy.addEventListener('change', (event) => {
+	resultTo = `${event.target.value}`;
 });
 
 search.addEventListener('input', updateValue);
 
-
 // update the value of the currency
-function updateValue (e){
-  searchValue = e.target.value;
+function updateValue(e) {
+	searchValue = e.target.value;
 }
-// call the function when clicked
+
+
 convert.addEventListener("click", getResults);
 
-// function for getResults
-function getResults(){
-  fetch(`${api}`)
-    .then(currency => {
-      return currency.json();
-    }).then(displayResults);
+// getReuslts function
+function getResults() {
+	fetch(`${api}`)
+		.then(currency => {
+			return currency.json();
+		}).then(displayResults);
 }
 
-// display the convertion to the user
-function displayResults(currency){
-  let fromRate = currency.rates[resultFrom];
-  let toRate = currency.rates[resultTo];
-  finalValue.innerHTML = 
-  ((toRate / fromRate) * searchValue).toFixed(2);
-  finalAmount.style.display = "block";
+// convertion
+function displayResults(currency) {
+	let fromRate = currency.rates[resultFrom];
+	let toRate = currency.rates[resultTo];
+	finalValue.innerHTML =
+	((toRate / fromRate) * searchValue).toFixed(2);
+	finalAmount.style.display = "block";
 }
 
-// function for reset button
-function clearVal(){
-  window.location.reload();
-  document.getElementsByClassName("finalValue").innerHTML = ""; 
-}
+// reset for the currency
+function clearVal() {
+	window.location.reload();
+	document.getElementsByClassName("finalValue").innerHTML = "";
+};
 
 
